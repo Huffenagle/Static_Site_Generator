0ec3866fd7d95629f3a11706864da43010dc6385
Huffenagle <Huffenagle@Gmail.com>
2024-09-18 16:18:10 -0400

    Not gonna lie I needed help for the last few steps.  Completed generate content recursively function.  Need to look into os path functions more closely and see what they're about.

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,11 +1,26 @@
 import os
+from pathlib import Path
 from blockline_markdown import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            print(dest_path)
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
 def generate_page(from_path, template_path, dest_path):
+    
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
     from_file = open(from_path, 'r')
     markdown = from_file.read()
     from_file.close()
+    
     template_file = open(template_path, 'r')
     template = template_file.read()
     template_file.close()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 from copystatic import copy_files_recursive
 
 dir_path_static = "./static"
@@ -17,9 +17,7 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    from_path = "./content/index.md"
-    template_path = "./template.html"
-    dest_path = "./public/index.html"
-    generate_page(from_path, template_path, dest_path)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, dir_path_template, dir_path_public)
 
 main()
